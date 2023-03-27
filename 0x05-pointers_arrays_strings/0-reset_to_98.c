35a647b1a0d3e0dec7b4c0217b935e70f35f3efe
root <sallykopudo@gmail.com>
2023-03-27 08:56:38 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98- takes a pointer to an int as parameter
- *  		and updates the value it points
+ *		and updates the value it points 
  * @n: Function parameter
  *
  * Return:Always 0
