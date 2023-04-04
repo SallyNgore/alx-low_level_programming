4239fffe84f3ffe786ab18e7a03c4ee30f6c4758
root <sallykopudo@gmail.com>
2023-04-04 09:39:22 -0700

    4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,23 +11,17 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
-	int j;
-	int c = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		if (s[i] != 32)
+		for (i = 0 ; accept[i] ; i++)
 		{
-			for (j = 0; accept[j] != '\0'; j++)
+			if (*s == accept[i])
 			{
-				if (s[i] == accept[j])
-				c++;
+				return (s);
 			}
 		}
-		else
-		{
-			return (0);
-		}
+		s++;
 	}
-	return (0);
+	return ('\0');
 }
