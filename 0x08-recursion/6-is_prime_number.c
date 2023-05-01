c635d268bab514b1c2f4528f780e056e4fb760f5
root <sallykopudo@gmail.com>
2023-05-01 04:25:09 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 6-main.c 6-is_prime_number.c -o 6-prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * is_prime_number - determine if a number is a prime number
- * @n: int number
- * Return: 1 if prime, 0 otherwise
- */
-
-int is_prime_number(int n)
-{
-	if (n < 2)
-		return (0);
-	if (n < 4)
-		return (1);
-	return (hai(n, 2));
-}
-
-/**
  * _sqrt - return square root of number
  * @x: number
  * @i: number incrementer acting as divisor
@@ -48,4 +33,19 @@ int hai(int n, int d)
 		return (1);
 	else
 		return (hai(n, d + 1));
+}
+
+/**
+ * is_prime_number - determine if a number is a prime number
+ * @n: int number
+ * Return: 1 if prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	if (n < 4)
+		return (1);
+	return (hai(n, 2));
 }
