91257cff36aabf2b505d7e41ff2a9792cd8b9c10
SallyNgore <sallykopudo@gmail.com>
2023-07-27 02:00:22 -0700

    argc, argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE_NAME
+#define HEADER_FILE_NAME
+
+int main(int __attribute__((__unused__)) argc, char *argv[]);
+int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
+
+#endif
